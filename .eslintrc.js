5e8dc15a6042415809d308368f5f5cc80c71de56
Kevin Wessa <kjwessa@gmail.com>
2023-06-23 10:14:58 -0500

    build: add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+  },
+};
