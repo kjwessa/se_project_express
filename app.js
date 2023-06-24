f01766e7a8f47dcb81392700e32e8609bb1a1a4f
Kevin Wessa <kjwessa@gmail.com>
2023-06-24 10:42:37 -0500

    fix: repair broken connection to MongoDB with error handling

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/wtwr_db",
+  (r) => {
+    console.log("connected to DB", r);
+  },
+  (e) => console.log("error connecting to DB", e)
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
