1392ef06d86e451a8e83669afc7d3b63c58065d1
Kevin Wessa <kjwessa@gmail.com>
2023-06-26 11:56:10 -0500

    chores: remove unneeded code from users.js

--- controllers/user.js
+++ controllers/user.js
@@ -21,32 +21,6 @@ function handleCatchMethod(req, res, err) {
     .send({ message: "An error occurred on the server", err });
 }
 
-// function handleCatchMethod(req, res, err) {
-//   if (err.name === "ValidationError" || err.name === "AssertionError") {
-//     return res.status(errorCode400).send({
-//       message:
-//         "Invalid data passed to the methods for creating a user or invalid ID passes to the params.",
-//     });
-//   }
-
-//   if (err.name === "CastError") {
-//     return res.status(errorCode404).send({
-//       message:
-//         "The requested id is invalid or the request was sent to a non-existent address",
-//     });
-//   }
-
-//   if (err.message === "NotFound") {
-//     return res.status(errorCode400).send({
-//       message: "There is no user with the requested id",
-//     });
-//   }
-
-//   return res
-//     .status(errorCode500)
-//     .send({ message: "An error occurred on the server", err });
-// }
-
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
@@ -54,17 +28,6 @@ const getUsers = (req, res) => {
       handleCatchMethod(req, res, err);
     });
 };
-
-// const getUser = (req, res) => {
-//   const { itemId } = req.params;
-
-//   User.findById(itemId)
-//     .orFail(new Error("NotFound"))
-//     .then((user) => res.status(200).send({ data: user }))
-//     .catch((err) => {
-//       handleCatchMethod(req, res, err);
-//     });
-// };
 
 const getUser = (req, res) => {
   const { itemId } = req.params;
