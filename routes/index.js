9a5d40d030ec4e061ad56ef8ca37831578f4d04c
Kevin Wessa <kjwessa@gmail.com>
2023-06-27 09:35:12 -0500

    refactor: update index.js to use error code variables

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 const router = require("express").Router();
 const User = require("./user");
 const clothingItem = require("./clothingItem");
+const { errorCode404 } = require("../utils/errors");
 
 router.use("/items", clothingItem);
 router.use("/users", User);
 
 router.use((req, res) => {
-  res
-    .status(404)
-    .send({
-      message:
-        "There is no clothing item with the requested id, or the request was sent to a non-existent address",
-    });
+  res.status(errorCode404).send({
+    message:
+      "There is no clothing item with the requested id, or the request was sent to a non-existent address",
+  });
 });
 
 module.exports = router;
