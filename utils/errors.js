5c77d37b8e0791c939878e2a6527b050f594cd5d
Kevin Wessa <kjwessa@gmail.com>
2023-07-04 11:32:35 -0500

    feat: create an object with error code variables

--- utils/errors.js
+++ utils/errors.js
@@ -1,62 +1,92 @@
-const errorCode400 = 400;
-const errorCode404 = 404;
-const errorCode500 = 500;
+// const errorCode400 = 400;
+// const errorCode404 = 404;
+// const errorCode500 = 500;
+
+const ERROR_CODES = {
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  NotFound: 404,
+  AlreadyExistsError: 409,
+  ServerError: 500,
+  MongoError: 11000,
+};
 
 const handleOnFailError = () => {
   const error = new Error("User not found");
-  error.statusCode = errorCode404;
+  error.statusCode = ERROR_CODES.NotFound;
   throw error;
 };
 
-function handleError(res, err) {
-  if (
-    err.name === "ValidationError" ||
-    err.name === "AssertionError" ||
-    err.name === "CastError"
-  ) {
-    return res.status(errorCode400).send({
-      message:
-        "Invalid data passed to the methods for creating an item or updating an item, or invalid ID passed to the params.",
+const handleError = (res, err) => {
+  if (err.name === "ValidationError" || err.name === "CastError") {
+    res
+      .status(ERROR_CODES.BadRequest)
+      .send({ message: "Bad Request, Invalid input" });
+  } else if (err.message === "Incorrect email or password") {
+    res
+      .status(ERROR_CODES.Unauthorized)
+      .send({ message: "You are not authorized to do this" });
+  } else if (err.statusCode === 404) {
+    res.status(ERROR_CODES.NotFound).send({ message: "Item not found" });
+  } else if (err.code === 11000) {
+    res.status(ERROR_CODES.AlreadyExistsError).send({
+      message: "Email address is already being used, please try another email.",
     });
+  } else {
+    res
+      .status(ERROR_CODES.ServerError)
+      .send({ message: "Something went wrong" });
   }
+};
 
-  if (err.statusCode && err.statusCode === 404) {
-    return res.status(err.statusCode).send({
-      message:
-        "There is no clothing item with the requested id, or the request was sent to a non-existent address.",
-    });
-  }
+// function handleError(res, err) {
+//   if (
+//     err.name === "ValidationError" ||
+//     err.name === "AssertionError" ||
+//     err.name === "CastError"
+//   ) {
+//     return res.status(errorCode400).send({
+//       message:
+//         "Invalid data passed to the methods for creating an item or updating an item, or invalid ID passed to the params.",
+//     });
+//   }
 
-  return res
-    .status(errorCode500)
-    .send({ message: "An error has occurred on the server", err });
-}
+//   if (err.statusCode && err.statusCode === 404) {
+//     return res.status(err.statusCode).send({
+//       message:
+//         "There is no clothing item with the requested id, or the request was sent to a non-existent address.",
+//     });
+//   }
 
-function handleCatchMethod(req, res, err) {
-  if (err.name === "ValidationError" || err.name === "AssertionError") {
-    return res.status(errorCode400).send({
-      message:
-        "Invalid data passed to the methods for creating a user or invalid ID passes to the params.",
-    });
-  }
+//   return res
+//     .status(errorCode500)
+//     .send({ message: "An error has occurred on the server", err });
+// }
 
-  if (err.name === "CastError") {
-    return res.status(errorCode404).send({
-      message:
-        "There is no user with the requested id, or the request was sent to a non-existent address",
-    });
-  }
+// function handleCatchMethod(req, res, err) {
+//   if (err.name === "ValidationError" || err.name === "AssertionError") {
+//     return res.status(errorCode400).send({
+//       message:
+//         "Invalid data passed to the methods for creating a user or invalid ID passes to the params.",
+//     });
+//   }
 
-  return res
-    .status(errorCode500)
-    .send({ message: "An error occurred on the server", err });
-}
+//   if (err.name === "CastError") {
+//     return res.status(errorCode404).send({
+//       message:
+//         "There is no user with the requested id, or the request was sent to a non-existent address",
+//     });
+//   }
+
+//   return res
+//     .status(errorCode500)
+//     .send({ message: "An error occurred on the server", err });
+// }
 
 module.exports = {
-  errorCode400,
-  errorCode404,
-  errorCode500,
+  ERROR_CODES,
   handleError,
-  handleCatchMethod,
+  // handleCatchMethod,
   handleOnFailError,
 };
