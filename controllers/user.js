9af63e4a1e4790bd3d97adfb9c79eb4fa2d6b094
Kevin Wessa <kjwessa@gmail.com>
2023-07-05 10:43:27 -0500

    fix: repair createUser controller to handle invalid email and password

--- controllers/user.js
+++ controllers/user.js
@@ -29,19 +29,61 @@ const {
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      User.create({ name, avatar, email, password: hash })
-        .then((user) => {
-          const userData = user.toObject();
-          delete userData.password;
-          res.status(201).send({ data: userData });
+  User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        res.status(ERROR_CODES.AlreadyExistsError).json({
+          message: "Email address is already in use",
+        });
+        return;
+      }
+
+      bcrypt
+        .hash(password, 10)
+        .then((hash) => {
+          User.create({ name, avatar, email, password: hash })
+            .then((user) => {
+              const userData = user.toObject();
+              delete userData.password;
+              res.status(201).json({ data: userData });
+            })
+            .catch((err) => handleError(res, err));
         })
         .catch((err) => handleError(res, err));
     })
     .catch((err) => handleError(res, err));
 };
+// const createUser = (req, res) => {
+//   const { name, avatar, email, password } = req.body;
+
+//   if (!password) {
+//     res
+//       .status(ERROR_CODES.Unauthorized)
+//       .send({ message: "Password is required" });
+//   }
+
+//   return bcrypt
+//     .hash(password, 10)
+//     .then((hash) => User.create({ name, avatar, email, password: hash }))
+//     .then((user) => {
+//       res.send({ name, avatar, _id: user._id, email: user.email });
+//     })
+//     .catch((error) => {
+//       if (error.name === "ValidationError") {
+//         res
+//           .status(ERROR_CODES.MongoError)
+//           .send({ message: "Invalid data provided" });
+//       } else if (error.code === 11000) {
+//         res
+//           .status(ERROR_CODES.AlreadyExistsError)
+//           .send({ message: "Email already exists in database" });
+//       } else {
+//         res
+//           .status(ERROR_CODES.ServerError)
+//           .send({ message: "An error has occurred on the server" });
+//       }
+//     });
+// };
 
 const getCurrentUser = (req, res) => {
   User.findById(req.user._id)
@@ -71,7 +113,7 @@ const login = (req, res) => {
 
   if (!email || !password) {
     return res
-      .status(ERROR_CODES.BAD_REQUEST)
+      .status(ERROR_CODES.Unauthorized)
       .send({ message: "You are not authorized to do this" });
   }
   return User.findUserByCredentials(email, password)
