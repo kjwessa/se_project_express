22b2b19cad10dbea767ba7cd85532ea746574090
Kevin Wessa <kjwessa@gmail.com>
2023-08-10 11:24:07 -0500

    refactor: rebuild login with error middleware

--- controllers/user.js
+++ controllers/user.js
@@ -29,6 +29,41 @@ const createUser = (req, res, next) => {
     });
 };
 
+// const createUser = (req, res) => {
+//   const { name, avatar, email, password } = req.body;
+
+//   if (!email || !password) {
+//     res.status(ERROR_CODES.BadRequest).json({
+//       message: "Email and password are required",
+//     });
+//     return;
+//   }
+
+//   User.findOne({ email })
+//     .then((existingUser) => {
+//       if (existingUser) {
+//         res.status(ERROR_CODES.AlreadyExistsError).json({
+//           message: "Email address is already in use",
+//         });
+//         return;
+//       }
+
+//       bcrypt
+//         .hash(password, 10)
+//         .then((hash) => {
+//           User.create({ name, avatar, email, password: hash })
+//             .then((user) => {
+//               const userData = user.toObject();
+//               delete userData.password;
+//               res.status(201).json({ data: userData });
+//             })
+//             .catch((err) => handleError(res, err));
+//         })
+//         .catch((err) => handleError(res, err));
+//     })
+//     .catch((err) => handleError(res, err));
+// };
+
 const getCurrentUser = (req, res, next) => {
   const { _id: userId } = req.user;
 
@@ -44,6 +79,15 @@ const getCurrentUser = (req, res, next) => {
     });
 };
 
+// const getCurrentUser = (req, res) => {
+//   User.findById(req.user._id)
+//     .orFail(() => {
+//       handleOnFailError();
+//     })
+//     .then((user) => res.status(200).send({ data: user }))
+//     .catch((err) => handleError(res, err));
+// };
+
 const updateCurrentUser = (req, res) => {
   const { name, avatar } = req.body;
   User.findByIdAndUpdate(
@@ -58,23 +102,47 @@ const updateCurrentUser = (req, res) => {
     .catch((err) => handleError(res, err));
 };
 
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res
-      .status(ERROR_CODES.Unauthorized)
-      .send({ message: "You are not authorized to do this." });
-  }
-
-  return User.findUserByCredentials(email, password)
+  User.findUserByCredentials(email, password)
     .then((user) => {
-      res.send({
-        token: jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" }),
+      if (!user) {
+        return next(new UnauthorizedError("Email or Password not found"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return next(new UnauthorizedError("Email or Password not found"));
+        }
+
+        const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+          expiresIn: "7d",
+        });
+        res.send({ token });
       });
     })
-    .catch((err) => handleError(res, err));
+    .catch((error) => {
+      next(error);
+    });
 };
+
+// const login = (req, res) => {
+//   const { email, password } = req.body;
+
+//   if (!email || !password) {
+//     return res
+//       .status(ERROR_CODES.Unauthorized)
+//       .send({ message: "You are not authorized to do this." });
+//   }
+
+//   return User.findUserByCredentials(email, password)
+//     .then((user) => {
+//       res.send({
+//         token: jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" }),
+//       });
+//     })
+//     .catch((err) => handleError(res, err));
+// };
 
 module.exports = {
   createUser,
