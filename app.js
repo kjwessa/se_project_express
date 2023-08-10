7b117fc9f72a1e9c5c5530d92a1e93453daade69
Kevin Wessa <kjwessa@gmail.com>
2023-08-10 11:48:01 -0500

    feat: add celebrate library to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
+const { errors } = require("celebrate");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -18,6 +19,7 @@ mongoose.connect(
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
