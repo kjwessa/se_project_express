5127303e35bc2a0dbd9b21ec06adce2abb19e2a9
Kevin Wessa <kjwessa@gmail.com>
2023-08-11 09:04:48 -0500

    feat: build validateUrl and validateEmail functions

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,1 +1,21 @@
 const { Joi, celebrate, Segments } = require("celebrate");
+const validator = require("validator");
+
+const validateUrl = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+const validateEmail = (value, helpers) => {
+  if (validator.isEmail(value)) {
+    return value;
+  }
+  return helpers.error("string.email");
+};
+
+module.exports = {
+  validateUrl,
+  validateEmail,
+};
