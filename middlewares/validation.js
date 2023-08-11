263d89174582ae6aed354b0576b0e2d391cd195a
Kevin Wessa <kjwessa@gmail.com>
2023-08-11 09:06:55 -0500

    feat: build validateClothingItem function

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -15,7 +15,25 @@ const validateEmail = (value, helpers) => {
   return helpers.error("string.email");
 };
 
+const validateClothingItem = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    imageUrl: Joi.string().required().custom(validateUrl).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'The "imageUrl" field must be a valid url',
+    }),
+    weather: Joi.string().required().valid("hot", "warm", "cold").messages({
+      "string.empty": 'The "weather" field must be filled in',
+    }),
+  }),
+});
+
 module.exports = {
   validateUrl,
   validateEmail,
+  validateClothingItem,
 };
