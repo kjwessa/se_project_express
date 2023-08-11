bff87cc903c77be5741606a623090751739a9498
Kevin Wessa <kjwessa@gmail.com>
2023-08-11 09:00:24 -0500

    fix: change the order of the import statements in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const routes = require("./routes");
-const errorHandler = require("./middlewares/errorHandler");
 const { errors } = require("celebrate");
+const errorHandler = require("./middlewares/errorHandler");
+const routes = require("./routes");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -18,8 +18,14 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+
+// our routes
 app.use(routes);
+
+// celebrate error handler
 app.use(errors());
+
+// our centralized handler
 app.use(errorHandler);
 
 app.listen(PORT, () => {
---
+++ middlewares/validation.js
@@ -0,0 +1,1 @@
+const { Joi, celebrate, Segments } = require("celebrate");
