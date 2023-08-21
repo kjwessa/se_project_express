12fdb564f8186bafabe0146615111bc7cd9a60c1
Kevin Wessa <kjwessa@gmail.com>
2023-08-21 09:01:33 -0500

    chores: update to port 3000

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const errorHandler = require("./middlewares/errorHandler");
 const routes = require("./routes");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect(
