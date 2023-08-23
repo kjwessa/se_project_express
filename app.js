aeb20a17cb76a2c8e2dfc49c6b7fba3f83cbd3d2
Kevin Wessa <kjwessa@gmail.com>
2023-08-23 14:12:18 -0500

    build: add dotenv to project dependencies

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const express = require("express");
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const JWT_SECRET = "some-secret-key";
+const JWT_SECRET = process.env;
 
 module.exports = {
   JWT_SECRET,
