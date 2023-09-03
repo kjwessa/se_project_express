fd3884fda48866549cc295a7326b96decc553d56
Kevin Wessa <kjwessa@gmail.com>
2023-09-03 12:35:57 -0500

    feat: add celebrate validation schemes to suitable routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
-
 const { getCurrentUser, updateCurrentUser } = require("../controllers/user");
+const { validateProfileAvatar } = require("../middlewares/validation");
 
 router.get("/me", auth.handleAuthError, getCurrentUser);
-router.patch("/me", auth.handleAuthError, updateCurrentUser);
+router.patch(
+  "/me",
+  auth.handleAuthError,
+  validateProfileAvatar,
+  updateCurrentUser
+);
 
 module.exports = router;
