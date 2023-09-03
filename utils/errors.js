a70c8f4858a30ad45ccf2a5a98dd4d3becf67b79
Kevin Wessa <kjwessa@gmail.com>
2023-09-03 12:03:16 -0500

    chores: remove unneeded code from errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -8,38 +8,6 @@ const ERROR_CODES = {
   MongoError: 11000,
 };
 
-const handleOnFailError = () => {
-  const error = new Error("User not found");
-  error.statusCode = ERROR_CODES.NotFound;
-  throw error;
-};
-
-const handleError = (res, err) => {
-  if (err.name === "ValidationError" || err.name === "CastError") {
-    res
-      .status(ERROR_CODES.BadRequest)
-      .send({ message: "Bad Request, Invalid input!" });
-  } else if (err.message === "Incorrect email or password") {
-    res
-      .status(ERROR_CODES.Unauthorized)
-      .send({ message: "You are not authorized to do this" });
-  } else if (err.statusCode === ERROR_CODES.Forbidden) {
-    res.status(ERROR_CODES.Forbidden).send({ message: "Access forbidden" });
-  } else if (err.statusCode === ERROR_CODES.NotFound) {
-    res.status(ERROR_CODES.NotFound).send({ err, message: "Item not found" });
-  } else if (err.code === ERROR_CODES.MongoError) {
-    res.status(ERROR_CODES.AlreadyExistsError).send({
-      message: "Email address is already being used, please try another email.",
-    });
-  } else {
-    res
-      .status(ERROR_CODES.ServerError)
-      .send({ message: "Something went wrong" });
-  }
-};
-
 module.exports = {
   ERROR_CODES,
-  handleError,
-  handleOnFailError,
 };
